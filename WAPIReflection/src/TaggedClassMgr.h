0269b5a5ec6b9b479c35dcbdb033c9580fb422fd
osdever <krasota156@gmail.com>
2020-05-22 20:20:38 +0300

    Generalized the code

---
+++ WAPIReflection/src/TaggedClassMgr.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <wapirefl/Assembly.h>
+#include <wapirefl/Class.h>
+
+// Represents a reference to a class instance.
+// Provides methods to invoke arbitrary methods of said class.
+class InstanceRef
+{
+public:
+	// Constructs an InstanceRef from the specified class.
+	InstanceRef(const WAPIReflection::Class& class_)
+		: mClass(class_), mInstance(class_.constructNoParams())
+	{}
+
+	// Invokes a method of the class this instance is bound to.
+	// Does nothing if the specified method does not exist.
+	// NOTE: you are responsible for checking arguments' validity!
+	template<typename... Args>
+	void InvokeIfValid(const std::string& method, Args... args)
+	{
+		auto pMethod = mClass.findMethod(method);
+		if (pMethod)
+			pMethod->invoke<void, Args...>(mInstance, args...);
+	}
+
+private:
+	WAPIReflection::Class mClass;
+	void* mInstance;
+};
+
+// Manages a list of instances of classes distinguished by inheritance from the Tag type.
+// Provides methods to add new classes, find them in WAPIRefl assemblies and invoke methods on all instances.
+template<class Tag>
+class TaggedClassMgr
+{
+public:
+	// Checks if T inherits from TSystemTag: if it does, constructs an instance of T
+	// and adds it to the instance list. If it doesn't, does nothing.
+	template<typename T>
+	void Add() {
+		auto& type = typeof<T>();
+		if (type.kind() == WAPIReflection::TypeKind::Class)
+		{
+			auto& cl = type.asClass();
+
+			if (cl.baseClass() && *cl.baseClass() == typeof<Tag>())
+				AddInternal(cl);
+		}
+	}
+
+	// Constructs all classes inheriting from Tag in the specified assembly.
+	// Adds the resulting instances to the internal list.
+	void AddFromAssembly(WAPIReflection::Assembly& assembly)
+	{
+		for (auto& type : assembly.getAllTypes())
+		{
+			if (type.kind() == WAPIReflection::TypeKind::Class)
+			{
+				auto& cl = type.asClass();
+
+				if (cl.baseClass() && *cl.baseClass() == typeof<Tag>())
+					AddInternal(cl);
+			}
+		}
+	}
+
+protected:
+	// Invokes a method on all instances with the specified parameters.
+	// If an instance's class does not contain such method, it's ignored.
+	template<typename... Args>
+	void InvokeOnAll(const std::string& method, Args... args)
+	{
+		for (auto& sys : mInstances)
+			sys.InvokeIfValid(method, args...);
+	}
+
+	// Constructs a class instance without Tag checks.
+	void AddInternal(const WAPIReflection::Class& type)
+	{
+		std::printf(" => %s\n", type.name().c_str());
+		mInstances.push_back(InstanceRef(type));
+	}
+
+	std::vector<InstanceRef> mInstances;
+};
--- WAPIReflection/src/main.cpp
+++ WAPIReflection/src/main.cpp
@@ -1,10 +1,23 @@
 #include "SystemManager.h"
 
+class Plak : public ISystem
+{
+public:
+	void OnTextMessage(const char* mesg);
+};
+
+void Plak::OnTextMessage(const char* mesg)
+{
+	std::printf("[Plak] Message received: %s\n", mesg);
+}
+
 int main()
 {
 	SystemManager mgr;
 
-	mgr.AddSystemsFromAssembly(WAPIReflection::Assembly::local());
+	mgr.AddFromAssembly(WAPIReflection::Assembly::local());
+	//mgr.AddSystem<Plak>();
+
 	mgr.OnInitFinished();
 
 	mgr.OnTextMessage("Messaging!");
